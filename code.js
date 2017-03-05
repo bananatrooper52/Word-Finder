2d3d204fe0bbb219845e162fcbe2fc1c3bc9200f
bananatrooper52 <bananatrooper52@gmail.com>
2017-03-04 15:13:55 -1000

    show match count
    show the user how many matches their search got

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 var wordsloc = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt";
 var wordstring;
 var words;
+var blanks = ["-", ".", " "];
 
 $.ajax({
   async: true,
@@ -24,20 +25,21 @@ function showResults() {
   $("#inputword").val("");
 
   var results = [];
-  $("#answers").html("<h3>Possible words: </h3>");
 
   for(var i = 0; i < words.length; i++) {
     var add = true;
-    if(words[i].length != word.length) continue;
+    if(words[i].replace("'", "").length != word.replace("'", "").length) continue;
     for(var j = 0; j < word.length; j++) {
       if(word.charAt(j) == "-") continue;
-      if(words[i].charAt(j) != word.charAt(j)) {
+      if(words[i].replace("'", "").charAt(j) != word.replace("'", "").charAt(j)) {
         add = false;
         break;
       }
     }
     if(add) results.push(words[i]);
   }
+
+  $("#answers").html("<h3>Possible words (" + results.length + " matches): </h3>");
 
   for(var i = 0; i < results.length; i++) {
     $("#answers").append("<p>" + results[i] + "</p>");
